0e28d16cee51da77e302d1ee3d3d99d8998c528c
Martin Unger <martin-unger2@live.at>
2024-06-05 23:20:16 +0200

    added creat new todo tested with postman

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.contrib.auth.models import User
+from datetime import date
 
 class Todo(models.Model):
     title = models.CharField(max_length=30)
@@ -8,6 +9,12 @@ class Todo(models.Model):
     created_at = models.DateField(default=datetime.date.today)
     user = models.ForeignKey(
         User,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        default=None
     )
+    
+    def time_passed(self):
+        today = date.today()
+        delta = today - self.created_at
+        return delta
 
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,24 @@
+from telnetlib import STATUS
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import viewsets
+from django.core import serializers
+
+from .serializers import TodoSerializer
 from .models import Todo
-from .serializers import TododSerializer
 
 class TodoViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Todo.objects.all().order_by('-created_at')
-    serializer_class = TododSerializer
-    permission_classes = [] #permissions.IsAuthenticated
+    serializer_class = TodoSerializer
+    permission_classes = [] # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(title= request.POST.get('title', ''), 
+                                  description= request.POST.get('description', ''),
+                                  user= request.user,
+                                )
+        serialized_obj = serializers.serialize('json', [todo, ]) 
+        return HttpResponse(serialized_obj, content_type='application/json')
