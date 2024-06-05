b643b13fd9e83337170751e31e2da4e7ca830ed1
Martin Unger <martin-unger2@live.at>
2024-06-05 20:13:37 +0200

    installed django rest framework

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from .models import Todo
+from .serializers import TododSerializer
 
-# Create your views here.
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TododSerializer
+    permission_classes = [] #permissions.IsAuthenticated
